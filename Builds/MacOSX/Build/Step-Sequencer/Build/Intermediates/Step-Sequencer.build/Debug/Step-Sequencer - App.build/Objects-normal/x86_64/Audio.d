0b7fb44f12d0ae0c995b5d89c965d28f892f72b2
Corey Ford <corey2.ford@live.uwe.ac.uk>
2018-07-06 16:07:05 +0100

    First commit - built folder structure

---
+++ Builds/MacOSX/Build/Step-Sequencer/Build/Intermediates/Step-Sequencer.build/Debug/Step-Sequencer - App.build/Objects-normal/x86_64/Audio.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/coreyford/Documents/Code-Projects/C++/Step-Sequencer/Source/audio/Audio.cpp \
+  /Users/coreyford/Documents/Code-Projects/C++/Step-Sequencer/Source/audio/Audio.h
