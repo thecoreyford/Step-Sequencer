d379ab96402a5371ffb05ae4fd97b5a59e362135
Corey Ford <corey2.ford@live.uwe.ac.uk>
2018-07-20 16:27:13 +0100

    Started work on MidiOutput class.

---
+++ Source/audio/MidiOut.h
@@ -0,0 +1,34 @@
+/*
+  ==============================================================================
+
+    MidiOut.h
+    Created: 20 Jul 2018 4:16:23pm
+    Author:  Corey Ford
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../gui/widgets/CustomToggle.h"
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+
+//TODO(corey2.ford!@live.uwe.ac.uk): come back here after refactoring CustomToggle
+
+namespace audio
+{
+    class MidiOut : gui::CustomToggle::Listener
+    {
+    public:
+        
+        void customToggleChanged(const bool state,
+                                    const int row,
+                                    const int column) override;
+        
+    private:
+        juce::MidiOutput _midiOutput;
+        
+    };
+} //namespace audio
